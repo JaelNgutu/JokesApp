4830e7fb6c80c748e27d6ff3d11f2447d2b6225b
JaelNgutu <Jaelngutu@gmail.com>
2021-12-23 02:23:46 +0300

    add jokes service

---
+++ src/main/java/com/example/jokesapp/services/JokesServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+
+    //3rd party dependency
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
